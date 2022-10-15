5972216f80fa409565002f329af4f622bcfc4b5f
smshayan <83094510+smshayan@users.noreply.github.com>
2022-10-15 17:51:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+ function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) ;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) ;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  console.log(hourDegrees)
+}
+
+setInterval(setDate, 1000);
+setDate();
